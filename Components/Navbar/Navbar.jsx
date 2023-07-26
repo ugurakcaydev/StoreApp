e26671f9c375817287dc30d7c859e1248ada5577
Taner ÜZÜM <uzumtaner.official@gmail.com>
2023-07-26 20:26:36 +0300

    navbar added

--- Components/Navbar/Navbar.jsx
+++ Components/Navbar/Navbar.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import "./navBar.css"
 
 function Navbar() {
   return (
     <div>
-        
+        <div className="navbarContainer">
+            <div className="logoDiv"> <img src="./Images/dropbox-svgrepo-com.svg" alt=""/> </div>
+            <div className="navigatorDiv">
+                <ul>
+                  <li>Anasayfa</li>
+                  <li>Ürünler</li>
+                  <li>Hakkımızda</li>
+                  <li>İletişim</li>
+                  <li>SSS</li>
+                  <li>Sepet</li>
+                </ul>
+            </div>
+            <div className="authContainer">
+            	<input type="button" value="Log in"/>
+                <input type="button" value="Register" />
+            </div>
+
+        </div>
     </div>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
+import Navbar from "../../Components/Navbar/Navbar";
 import Product from "../../Components/Products/Product";
 export default function Home() {
   return (
     <>
+      <Navbar/>
       <Product />
     </>
   );
