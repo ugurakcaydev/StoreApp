29e89939a3e6c86736d556909e01ce225b99160a
Taner ÜZÜM <uzumtaner.official@gmail.com>
2023-07-28 23:05:55 +0300

    basket page

--- Components/Basket/Basket.jsx
+++ Components/Basket/Basket.jsx
@@ -1,18 +1,82 @@
-import { useEffect } from "react";
-
-
+import { useEffect, useState } from "react";
+import './basket.css'
 
 const  Basket= () => {
-    if (typeof window !== 'undefined') {
-        // Perform localStorage action
-        const basketArray =JSON.parse(localStorage.getItem('basket')) 
-        console.log(basketArray);
+  // let basketArray
+  //   if (typeof window !== 'undefined') {
+  //       // Perform localStorage action
+  //       basketArray =JSON.parse(localStorage.getItem('basket')) 
+  //       console.log(basketArray);
+
+  //     }
+
+  	const [basketArray,setBasketArray]=useState([]);
+	const [quantity,setquantity]=useState([]);
+  useEffect(() => {
+    const fetchBasket = async () => {
+
+      //Verileri asenkron olarak yüklüyoruz
+      if (typeof window !== 'undefined') 
+      {
+        const basketFromLocal = localStorage.getItem('basket');
+        if(basketFromLocal)
+        {
+              // Perform localStorage action
+              const parseBasket =JSON.parse(basketFromLocal) 
+              setBasketArray(parseBasket)
+        }
       }
+    };
+
+    fetchBasket();
+  }, []);
+
+  const quantityCounter=(value,product) => {
+	if(value =='add'){
+		let quantity = Number(JSON.stringify(product.productQuantity))
+		
+		quantity++;
+		console.log(quantity)
+		localStorage.setItem('basket', JSON.stringify(product));
+	}
+	else if (value =='reduce'){
+		
+		console.log('-----')
+	}
+	return product;
+  }
    
    return (
-     <div>
-         Basket
-     </div>
+     <>
+         { basketArray.map((product,index) => {
+          return (
+          	<div key={index} className="basketProductContainer">
+				<div className="productImage"><img src="" alt="" /></div>
+				<div className="productName">{product.productName}</div>
+				<div className="productPrice">{product.productPrice}</div>
+				<div className="productDescription">{product.productDescription}</div>
+
+				<div className="quantityContainer">
+
+				
+				<div className="addbuton" onClick={()=>{quantityCounter("add",product)}}>+</div>
+				<div className="productQuantity">{product.productQuantity}</div>
+				<div className="deletebuton"onClick={()=>{quantityCounter("reduce",product)}}>-</div>
+				</div>
+			</div>)
+    
+         })
+		 
+          
+          
+            
+         }
+		
+
+
+
+
+     </>
    )
  }
  
